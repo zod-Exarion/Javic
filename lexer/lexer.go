356706a36747de4bc49fb1188c821f99f32adfd6
zod-Exarion <exalted.rajubiri@gmail.com>
2025-12-15 21:42:18 +0530

    Initial lexer: Tokenizer skeleton

---
+++ lexer/lexer.go
@@ -0,0 +1,8 @@
+// Package lexer is the lexer for Javic
+//
+// I need to fill this out later
+package lexer
+
+func Lex(content string) {
+	return
+}
---
+++ lexer/tokenlist.go
@@ -0,0 +1,113 @@
+package lexer
+
+const (
+	// Special tokens
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT  = "IDENT"  // variable names, e.g. x, total
+	NUMBER = "NUMBER" // numeric literals
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	ASTERISK = "*"
+	CARET    = "^"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
+	NEQ      = "<>"
+	EX       = "!"
+	POUND    = "$"
+	SQUOTE   = "'"
+
+	// Delimiters
+	COMMA     = ","
+	COLON     = ":"
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	NLINE     = "\\n"
+
+	// String
+	DQUOTE = `"`
+	STRING = "STRING"
+
+	// Keywords (common in QBASIC)
+	MOD    = "MOD"
+	NOT    = "NOT"
+	LET    = "LET"
+	PRINT  = "PRINT"
+	INPUT  = "INPUT"
+	IF     = "IF"
+	THEN   = "THEN"
+	ELSE   = "ELSE"
+	END    = "END"
+	FOR    = "FOR"
+	TO     = "TO"
+	NEXT   = "NEXT"
+	GOTO   = "GOTO"
+	GOSUB  = "GOSUB"
+	RETURN = "RETURN"
+	WHILE  = "WHILE"
+	WEND   = "WEND"
+	DIM    = "DIM"
+	REM    = "REM" // comment
+	SELECT = "SELECT"
+	CASE   = "CASE"
+	IS     = "IS"
+)
+
+var keywords = map[string]TokenType{
+	"LET":   LET,
+	"PRINT": PRINT,
+	"INPUT": INPUT,
+	"IF":    IF,
+	"THEN":  THEN,
+	"ELSE":  ELSE,
+	"END":   END,
+	"FOR":   FOR,
+	"TO":    TO,
+	"NEXT":  NEXT,
+	"NOT":   NOT,
+	"MOD":   MOD,
+
+	// WARN: Dangerous Territory
+	"GOTO":  GOTO,
+	"GOSUB": GOSUB,
+
+	"RETURN": RETURN,
+	"WHILE":  WHILE,
+	"WEND":   WEND,
+	"DIM":    DIM,
+	"REM":    REM,
+}
+
+var singleton = map[string]TokenType{
+	// Operators
+	"=":  ASSIGN,
+	"+":  PLUS,
+	"-":  MINUS,
+	"*":  ASTERISK,
+	"^":  CARET,
+	"/":  SLASH,
+	"<":  LT,
+	">":  GT,
+	"<>": NEQ,
+	"!":  EX,
+	"$":  POUND,
+	"'":  SQUOTE,
+
+	// String
+	`"`: DQUOTE,
+
+	// Delimiters
+	",":   COMMA,
+	":":   COLON,
+	";":   SEMICOLON,
+	"(":   LPAREN,
+	")":   RPAREN,
+	"\\n": NLINE,
+}
